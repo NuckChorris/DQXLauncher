22a9b1ad39e2fe59c687009a93b9cb86c7293885
Emma Lejeck <emma@nuck.moe>
2025-04-04 16:07:56 -0700

    Fix href extraction from Hiroba banners

--- DQXLauncher.Core/Hiroba/BannerImage.cs
+++ DQXLauncher.Core/Hiroba/BannerImage.cs
@@ -1,16 +1,20 @@
 ﻿using HtmlAgilityPack;
+using DQXLauncher.Core.Utils.WebClient;
+using System.Text.RegularExpressions;
 
 namespace DQXLauncher.Core.Hiroba;
 
-public class BannerImage
+public partial class BannerImage
 {
+    [GeneratedRegex(@"javascript:ctrLinkAction\('link=([^']+)'\);")]
+    private static partial Regex HrefFormat();
     public string Alt { get; set; } = string.Empty;
     public string Href { get; set; } = string.Empty;
     public string Src { get; set; } = string.Empty;
 
     public static async Task<List<BannerImage>> GetBanners()
     {
-        var httpClient = await Utils.WebClient.GetHttpClient("banners");
+        var httpClient = await WebClient.Get("banners");
 
         var doc = new HtmlDocument();
         var result = await httpClient.GetAsync("https://hiroba.dqx.jp/sc/rotationbanner");
@@ -19,11 +23,15 @@ public class BannerImage
         return doc.DocumentNode.SelectNodes("//li[contains(@class, 'slide')]/a").Select(slide =>
         {
             var img = slide.SelectSingleNode(".//img");
+            var match = HrefFormat().Match(slide.GetAttributeValue("href", string.Empty));
+            var href = match.Success ? match.Groups[1].Value : string.Empty;
+            //"javascript:ctrLinkAction('link=https://hiroba.dqx.jp/sc/topics/detail/39539f630a3b94d3ed61ea9d04c9bb05/');"
+
 
             return new BannerImage
             {
                 Alt = slide.GetAttributeValue("alt", string.Empty),
-                Href = slide.GetAttributeValue("href", string.Empty),
+                Href = href,
                 Src = img.GetAttributeValue("src", string.Empty)
             };
         }).ToList();
