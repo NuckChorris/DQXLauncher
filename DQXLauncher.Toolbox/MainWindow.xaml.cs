1a71ec9cbb3d00cb2977ee81f31506fa1221d2f4
Emma Lejeck <emma@nuck.moe>
2025-04-10 23:46:51 -0700

    Add DQXLauncher.Toolbox project for UI to explore DQX files

---
+++ DQXLauncher.Toolbox/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace DQXLauncher.Toolbox
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            ContentFrame.Navigate(typeof(HomePage));
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                ContentFrame.Navigate(typeof(SettingsPage));
+            }
+            else if (args.SelectedItem is NavigationViewItem selectedItem)
+            {
+                switch (selectedItem.Tag)
+                {
+                    case "HomePage":
+                        ContentFrame.Navigate(typeof(HomePage));
+                        break;
+                    case "SaveExplorerPage":
+                        ContentFrame.Navigate(typeof(SaveExplorerPage));
+                        break;
+                }
+            }
+        }
+    }
+}
