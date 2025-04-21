203b7181d14461d37428f12e3a91a76cb3cdeecc
Emma Lejeck <emma@nuck.moe>
2025-04-20 18:26:37 -0700

    feat(core): Add SavedPlayerLoginStrategy
    This strategy takes a saved player token and performs the flow. It
    also helpfully provides the username when asking for a password, so
    it can also be utilized for looking up the sqexid of a token.

--- DQXLauncher.Core/Game/LoginStrategy/LoginStep.cs
+++ DQXLauncher.Core/Game/LoginStrategy/LoginStep.cs
@@ -1,9 +1,13 @@
 ﻿namespace DQXLauncher.Core.Game.LoginStrategy;
 
 public abstract record LoginStep;
+
 public record AskUsernamePassword : LoginStep;
-public record AskPassword : LoginStep;
-public record AskOtp : LoginStep;
+
+public record AskPassword(string Username) : LoginStep;
+
+public record AskOtp(string Username) : LoginStep;
+
 public record AskEasyPlay : LoginStep;
 
 public partial record DisplayError(string Message, LoginStep? Then) : LoginStep;
---
+++ DQXLauncher.Core/Game/LoginStrategy/SavedPlayerLoginStrategy.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.Contracts;
+using DQXLauncher.Core.Utils.WebClient;
+
+namespace DQXLauncher.Core.Game.LoginStrategy;
+
+public class SavedPlayerLoginStrategy : LoginStrategy
+{
+    private WebForm? _loginForm;
+    private Type? _expectedActionType;
+
+    public virtual async Task<LoginStep> Step(string token)
+    {
+        Contract.Assert(_expectedActionType == null);
+
+        _loginForm = await GetLoginForm(new Dictionary<string, string>
+        {
+            { "dqxmode", "2" },
+            { "id", token }
+        });
+
+        var username = _loginForm.Fields["sqexid"];
+
+        _expectedActionType = typeof(PasswordAction);
+        return new AskPassword(username);
+    }
+
+    public virtual async Task<LoginStep> Step(PasswordAction action)
+    {
+        Contract.Assert(_expectedActionType == typeof(PasswordAction));
+        Contract.Assert(_loginForm is not null);
+
+        var web = await GetWebClient();
+
+        _loginForm.Fields["password"] = action.Password;
+
+        var response = await LoginResponse.FromHttpResponse(await web.SendFormAsync(_loginForm));
+
+        if (response.ErrorMessage is not null)
+        {
+            _loginForm = response.Form;
+            return new DisplayError(response.ErrorMessage, new AskPassword(response.Form.Fields["sqexid"]));
+        }
+
+        return new LoginCompleted();
+    }
+}
