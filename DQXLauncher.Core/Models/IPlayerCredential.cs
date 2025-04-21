e77a6b18be40c0ec3038b6ef4fc9bc38084cf0cb
Emma Lejeck <emma@nuck.moe>
2025-04-20 22:18:23 -0700

    feat(core): Add IPlayerCredential
    This is a base interface for OS credential stores. On Windows, there
    will be an implementation based on Windows.Security.Credentials. On
    Mac, we can use the Keychain. On Linux, we can use org.freedesktop.secrets

---
+++ DQXLauncher.Core/Models/IPlayerCredential.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+using OtpNet;
+
+namespace DQXLauncher.Core.Models;
+
+public interface IPlayerCredential<TSelf> where TSelf : IPlayerCredential<TSelf>
+{
+    public string Token { get; init; }
+    public string? Password { get; set; }
+    public string? TotpKey { get; set; }
+
+    public static abstract TSelf Load(string token);
+    public void Save();
+    public void Remove();
+    public static abstract ImmutableList<TSelf> GetAll();
+
+    public string ComputeTotp()
+    {
+        var totp = new Totp(Base32Encoding.ToBytes(TotpKey));
+        return totp.ComputeTotp();
+    }
+}
