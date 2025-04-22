f4415ec1a4e11bd177e1391d963a4a94d0524ddc
Emma Lejeck <emma@nuck.moe>
2025-04-22 00:10:00 -0700

    refactor(windows): Expose App.AppWindow instance

--- DQXLauncher.Windows/App.xaml.cs
+++ DQXLauncher.Windows/App.xaml.cs
@@ -8,37 +8,36 @@ using Microsoft.UI.Xaml;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace DQXLauncher.Windows
+namespace DQXLauncher.Windows;
+
+/// <summary>
+///     Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : Application
 {
+    public static Window? AppWindow;
+
     /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
+    ///     Initializes the singleton application object.  This is the first line of authored code
+    ///     executed, and as such is the logical equivalent of main() or WinMain().
     /// </summary>
-    public partial class App : Application
+    public App()
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
-        {
-            Paths.AppData = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
-                , "DQXLauncher");
-            Paths.Create();
-            ConfigFile.RootDirectory = LauncherSettings.Instance.SaveFolderPath;
-            this.InitializeComponent();
-        }
+        Paths.AppData = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
+            , "DQXLauncher");
+        Paths.Create();
+        ConfigFile.RootDirectory = LauncherSettings.Instance.SaveFolderPath;
+        InitializeComponent();
+    }
 
-        /// <summary>
-        /// Invoked when the application is launched.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs args)
-        {
-            _window = new MainWindow();
-            _window.Activate();
-        }
-
-        private Window? _window;
+    /// <summary>
+    ///     Invoked when the application is launched.
+    /// </summary>
+    /// <param name="args">Details about the launch request and process.</param>
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        AppWindow = new MainWindow();
+        AppWindow.Activate();
     }
 }
--- DQXLauncher.Windows/MainWindow.xaml.cs
+++ DQXLauncher.Windows/MainWindow.xaml.cs
@@ -1,13 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using Windows.Gaming.Input;
-using DQXLauncher.Core.Game.LoginStrategy;
 using DQXLauncher.Windows.Utils;
 using DQXLauncher.Windows.Views.Pages;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
 using WinUIEx;
 
 namespace DQXLauncher.Windows
@@ -59,25 +56,6 @@ namespace DQXLauncher.Windows
         private void Gamepad_GamepadRemoved(object sender, Gamepad gamepad)
         {
             if (_gamepads.Contains(gamepad)) _gamepads.Remove(gamepad);
-        }
-
-        private async void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            var login = new GuestLoginStrategy();
-            //var result = await login.Login(Username.Text, Password.Text);
-            Debug.WriteLine("result");
-            var dialog = new ContentDialog
-            {
-                Content = "result",
-                PrimaryButtonText = "OK"
-            };
-            await dialog.ShowAsync();
-
-            // Process process = new Process();
-            // process.StartInfo.FileName = "C:\\Program Files (x86)\\SquareEnix\\DRAGON QUEST X\\Boot\\DQXLauncher.exe";
-            // process.StartInfo.Arguments = "-StartupToken=" + this.GetStartupToken();
-            // Submit.Content = (GetTime() >>> 1).ToString();
-            // process.Start();
         }
     }
 }
