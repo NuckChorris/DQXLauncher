2df5a62e52bc205067ba8ebe5c36172665a64f26
Emma Lejeck <emma@nuck.moe>
2025-04-25 00:09:22 -0700

    fix(core): Precisely match ConfigFile output to offical launcher

--- DQXLauncher.Core/Game/ConfigFile/ConfigFile.cs
+++ DQXLauncher.Core/Game/ConfigFile/ConfigFile.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml.Linq;
+﻿using System.Text;
+using System.Xml;
+using System.Xml.Linq;
 using DQXLauncher.Core.Utils;
 
 namespace DQXLauncher.Core.Game.ConfigFile;
@@ -35,16 +37,40 @@ public abstract class ConfigFile
 
         await using var fileStream = new FileStream(Filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
         await using var obfuscatorStream = _obfuscatorFactory(fileStream);
+        using var reader = new StreamReader(obfuscatorStream, new UTF8Encoding(false));
 
-        Document = await Task.Run(() => XDocument.Load(obfuscatorStream));
+        Document = await Task.Run(() => XDocument.Load(reader));
     }
 
     public async Task SaveAsync()
     {
         await using var fileStream = new FileStream(Filename, FileMode.Truncate, FileAccess.Write, FileShare.Read);
         await using var obfuscatorStream = _obfuscatorFactory(fileStream);
-        await Task.Run(() => Document?.Save(obfuscatorStream));
-        await obfuscatorStream.FlushAsync();
+        await using var writer =
+            new StreamWriter(obfuscatorStream, new UTF8Encoding(false));
+        await writer.WriteAsync(await Serialize());
+        await writer.FlushAsync();
+    }
+
+    private async Task<string> Serialize()
+    {
+        var settings = new XmlWriterSettings
+        {
+            Indent = true,
+            IndentChars = "\t",
+            Async = true,
+            NewLineChars = "\n",
+            NewLineHandling = NewLineHandling.Replace,
+            Encoding = new UTF8Encoding(false),
+            NewLineOnAttributes = false
+        };
+        await using var ms = new MemoryStream();
+        await using var writer = XmlWriter.Create(ms, settings);
+        await Task.Run(() => Document?.Save(writer));
+        await writer.FlushAsync();
+        var stringResult = Encoding.UTF8.GetString(ms.ToArray());
+        // Precisely match the exact format of the original file, DQXLauncher is very brittle!
+        return stringResult.Replace(" />", "/>").Replace("utf-8", "UTF-8") + "\n";
     }
 
     private async Task EnsureCreated()
