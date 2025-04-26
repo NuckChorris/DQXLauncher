180a96b206832c624ab697b678b3465e37af4d08
Emma Lejeck <emma@nuck.moe>
2025-04-26 00:55:56 -0700

    feat(windows): Add PlayerListItem.GetLoginStrategy()

--- DQXLauncher.Windows/ViewModels/PlayerListViewModel.cs
+++ DQXLauncher.Windows/ViewModels/PlayerListViewModel.cs
@@ -2,16 +2,18 @@
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using DQXLauncher.Core.Game.LoginStrategy;
 using DQXLauncher.Core.Models;
 using DQXLauncher.Windows.Services;
 using Microsoft.UI.Xaml.Controls;
 
 namespace DQXLauncher.Windows.ViewModels;
 
-public class PlayerListItem
+public abstract class PlayerListItem
 {
     public virtual Symbol Icon => Symbol.Help;
     public virtual string Text => null!;
+    public abstract Task<LoginStrategy> GetLoginStrategy();
 }
 
 public class SavedPlayerItem : PlayerListItem
@@ -19,20 +21,34 @@ public class SavedPlayerItem : PlayerListItem
     public required SavedPlayer<PlayerCredential> Player { get; init; }
     public override Symbol Icon => Symbol.Contact;
     public override string Text => Player.Name ?? $"Player {Player.Number}";
+
+    public override async Task<LoginStrategy> GetLoginStrategy()
+    {
+        return await Player.GetLoginStrategy();
+    }
 }
 
-public class GuestPlayerItem : PlayerListItem
+public class NewPlayerItem : PlayerListItem
 {
     public override Symbol Icon => Symbol.AddFriend;
     public override string Text => "New Player";
+
+    public override Task<LoginStrategy> GetLoginStrategy()
+    {
+        return Task.FromResult(new NewPlayerLoginStrategy() as LoginStrategy);
+    }
 }
 
+/*
+ @TODO: Implement EasyPlayLoginStrategy
 public class TrialPlayerItem : PlayerListItem
 {
     public required TrialPlayer Player { get; init; }
     public override Symbol Icon => Symbol.Emoji2;
     public override string Text => Player.Token;
+    public override Task<LoginStrategy> GetLoginStrategy() => Task.FromResult(new EasyPlayLoginStrategy(Player.Token) as LoginStrategy);
 }
+*/
 
 public partial class PlayerListViewModel : ObservableObject
 {
@@ -61,6 +77,6 @@ public partial class PlayerListViewModel : ObservableObject
 
         foreach (var savedPlayer in PlayerList.Players) List.Add(new SavedPlayerItem { Player = savedPlayer });
 
-        if (PlayerList.Players.Count < 4) List.Add(new GuestPlayerItem());
+        if (PlayerList.Players.Count < 4) List.Add(new NewPlayerItem());
     }
 }
