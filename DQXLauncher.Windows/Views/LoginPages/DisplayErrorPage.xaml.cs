cf284d6da2742a5fd3b8e9142db05b3f6cda964a
Emma Lejeck <emma@nuck.moe>
2025-05-04 17:52:21 -0700

    feat(windows): Add Success/Failure screens

---
+++ DQXLauncher.Windows/Views/LoginPages/DisplayErrorPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.Contracts;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using DQXLauncher.Core.Game.LoginStrategy;
+using DQXLauncher.Windows.ViewModels;
+using Microsoft.UI.Xaml;
+
+namespace DQXLauncher.Windows.Views.LoginPages;
+
+public partial class DisplayErrorPage
+{
+    public LoginFrameViewModel ViewModel { get; set; }
+
+    public DisplayError Step
+    {
+        get
+        {
+            Contract.Assert(ViewModel.Step is not null);
+            Contract.Assert(ViewModel.Step is DisplayError);
+            return (DisplayError)ViewModel.Step;
+        }
+    }
+
+    public DisplayErrorPage()
+    {
+        InitializeComponent();
+        ViewModel = Ioc.Default.GetRequiredService<LoginFrameViewModel>();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        ViewModel.Backward(Step.Continue);
+    }
+}
---
+++ DQXLauncher.Windows/Views/LoginPages/LoginCompletedPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using DQXLauncher.Core.Game;
+using DQXLauncher.Core.Game.LoginStrategy;
+using DQXLauncher.Windows.ViewModels;
+using Microsoft.UI.Xaml;
+
+namespace DQXLauncher.Windows.Views.LoginPages;
+
+public partial class LoginCompletedPage
+{
+    public LoginFrameViewModel ViewModel { get; set; }
+    public PlayerListViewModel PlayerListViewModel { get; set; }
+    public Launcher Launcher { get; set; }
+
+    public LoginCompletedPage()
+    {
+        InitializeComponent();
+        ViewModel = Ioc.Default.GetRequiredService<LoginFrameViewModel>();
+        PlayerListViewModel = Ioc.Default.GetRequiredService<PlayerListViewModel>();
+        Launcher = Ioc.Default.GetRequiredService<Launcher>();
+    }
+
+    private async void LoginCompletedPage_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (ViewModel.Step is LoginCompleted step)
+        {
+            if (ViewModel.Strategy is NewPlayerLoginStrategy)
+            {
+                var player = PlayerListViewModel.PlayerList?.Add(step.Token!);
+                await PlayerListViewModel.PlayerList?.SaveAsync();
+            }
+
+            Launcher.SessionId = step.SessionId;
+            await Launcher.LaunchGame();
+        }
+        else
+        {
+            throw new InvalidOperationException("Invalid step");
+        }
+    }
+}
